3f2eb0a963852f2493abc85ce02d558b9322379d
Michał Górny <mgorny@gentoo.org>
2011-11-28 12:26:36 +0100

    Support 'pure' and 'noreturn'.

--- ccattr.h
+++ ccattr.h
@@ -22,9 +22,19 @@
 #	if __has_attribute(constant) /* XXX: we can't use const... */
 #		define _CCATTR_HAVE_CONST
 #	endif
+#	if __has_attribute(noreturn)
+#		define _CCATTR_HAVE_NORETURN
+#	endif
+#	if __has_attribute(pure)
+#		define _CCATTR_HAVE_PURE
+#	endif
 #else /* !__has_attribute -- gcc */
 #	if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 4)
 #		define _CCATTR_HAVE_CONST
+#		define _CCATTR_HAVE_NORETURN
+#	endif
+#	if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 96)
+#		define _CCATTR_HAVE_PURE
 #	endif
 #endif /* __has_attribute */
 
@@ -37,11 +47,35 @@
  * Const function must not access memory referenced by its arguments as well.
  * It must not call non-const functions (including malloc()).
  */
-
 #ifdef _CCATTR_HAVE_CONST
 #	define CCATTR_CONST __attribute__((const))
 #else
 #	define CCATTR_CONST
+#endif
+
+/**
+ * CCATTR_PURE
+ *
+ * Declare a function as `pure'. This means that the function does not examine
+ * any values except its parameters and global variables, and has no effects
+ * except its return value.
+ */
+#ifdef _CCATTR_HAVE_PURE
+#	define CCATTR_PURE __attribute__((pure))
+#else
+#	define CCATTR_PURE
+#endif
+
+/**
+ * CCATTR_NORETURN
+ *
+ * Declare a function as `return'. This means that the function does not ever
+ * return (e.g. uses exit(), exec*() or a similar function).
+ */
+#ifdef _CCATTR_HAVE_NORETURN
+#	define CCATTR_NORETURN __attribute__((noreturn))
+#else
+#	define CCATTR_NORETURN
 #endif
 
 #endif /*_CCATTR_H*/
