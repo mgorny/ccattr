e1ac122274b59632dc6ef4cc36e60d13e2cbd458
Michał Górny <mgorny@gentoo.org>
2011-11-28 10:45:31 +0100

    Support 'const' function attribute.

--- ccattr.h
+++ ccattr.h
@@ -18,4 +18,30 @@
  * Right now, ccattr supports GCC & clang compilers.
  */
 
+#ifdef __has_attribute /* clang */
+#	if __has_attribute(constant) /* XXX: we can't use const... */
+#		define _CCATTR_HAVE_CONST
+#	endif
+#else /* !__has_attribute -- gcc */
+#	if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 4)
+#		define _CCATTR_HAVE_CONST
+#	endif
+#endif /* __has_attribute */
+
+/**
+ * CCATTR_CONST
+ *
+ * Declare a function as `const'. This means that the function does not examine
+ * any values except its parameters and has no effects except its return value.
+ *
+ * Const function must not access memory referenced by its arguments as well.
+ * It must not call non-const functions (including malloc()).
+ */
+
+#ifdef _CCATTR_HAVE_CONST
+#	define CCATTR_CONST __attribute__((const))
+#else
+#	define CCATTR_CONST
+#endif
+
 #endif /*_CCATTR_H*/
