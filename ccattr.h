45ba91f0105dd0fd0cea247f0da817ad84f1c166
Michał Górny <mgorny@gentoo.org>
2011-11-28 10:06:52 +0100

    Initiate codebase.

---
+++ ccattr.h
@@ -0,0 +1,21 @@
+/* Portable wrappers around CC extended attributes
+ * (c) 2011 Michał Górny
+ * 2-clause BSD-licensed
+ */
+
+#pragma once
+#ifndef _CCATTR_H
+#define _CCATTR_H
+
+/**
+ * SECTION: ccattr
+ * @short_description: macros to use CC attributes portably
+ * @include: ccattr.h
+ *
+ * ccattr is a simple header file which directly checks for various extended CC
+ * attributes and exports them via macros in a portable manner.
+ *
+ * Right now, ccattr supports GCC & clang compilers.
+ */
+
+#endif /*_CCATTR_H*/
